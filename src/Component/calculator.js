6d50717133ac542361c6c0059b0d831967227a0b
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-05-25 11:12:42 -0600

    Added Logic Functions

--- src/Component/calculator.js
+++ src/Component/calculator.js
@@ -1,40 +1,49 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { total: 0, next: null, operation: null };
+    this.onClickBtn = this.onClickBtn.bind(this);
+  }
+
+  onClickBtn(event) {
+    this.setState((state) => calculate(state, event));
   }
 
   render() {
+    const { total, next, operation } = this.state;
     return (
       <div className="calculator disflex">
         <div className="solution">
-          <span>0</span>
+          <span>{total}</span>
+          <span>{next}</span>
+          <span>{operation}</span>
         </div>
         <div className="btn-display disflex">
           <div className="operators">
-            <button type="button">AC</button>
-            <button type="button">+/-</button>
-            <button type="button">%</button>
-            <button type="button">7</button>
-            <button type="button">8</button>
-            <button type="button">9</button>
-            <button type="button">4</button>
-            <button type="button">5</button>
-            <button type="button">6</button>
-            <button type="button">1</button>
-            <button type="button">2</button>
-            <button type="button">3</button>
-            <button type="button" className="two-btn">0</button>
-            <button type="button">.</button>
+            <button type="button" onClick={() => this.onClickBtn('AC')}>AC</button>
+            <button type="button" onClick={() => this.onClickBtn('+/-')}>+/-</button>
+            <button type="button" onClick={() => this.onClickBtn('%')}>%</button>
+            <button type="button" onClick={() => this.onClickBtn('7')}>7</button>
+            <button type="button" onClick={() => this.onClickBtn('8')}>8</button>
+            <button type="button" onClick={() => this.onClickBtn('9')}>9</button>
+            <button type="button" onClick={() => this.onClickBtn('4')}>4</button>
+            <button type="button" onClick={() => this.onClickBtn('5')}>5</button>
+            <button type="button" onClick={() => this.onClickBtn('6')}>6</button>
+            <button type="button" onClick={() => this.onClickBtn('1')}>1</button>
+            <button type="button" onClick={() => this.onClickBtn('2')}>2</button>
+            <button type="button" onClick={() => this.onClickBtn('3')}>3</button>
+            <button type="button" onClick={() => this.onClickBtn('0')} className="two-btn">0</button>
+            <button type="button" onClick={() => this.onClickBtn('.')}>.</button>
           </div>
           <div className="orange-btn">
-            <button type="button">/</button>
-            <button type="button">*</button>
-            <button type="button">-</button>
-            <button type="button">+</button>
-            <button type="button">=</button>
+            <button type="button" onClick={() => this.onClickBtn('/')}>/</button>
+            <button type="button" onClick={() => this.onClickBtn('*')}>*</button>
+            <button type="button" onClick={() => this.onClickBtn('-')}>-</button>
+            <button type="button" onClick={() => this.onClickBtn('+')}>+</button>
+            <button type="button" onClick={() => this.onClickBtn('=')}>=</button>
           </div>
         </div>
       </div>
